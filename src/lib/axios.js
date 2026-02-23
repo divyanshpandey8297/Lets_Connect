137ff741f73e13a1bb4786177eb87634ca0403b5
divyansh pandey <divyansh@divyanshs-MacBook-Air.local>
2026-02-24 00:43:43 +0530

    change the code for production

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,10 +1,18 @@
+// import axios from "axios";
+
+// export const axiosInstance=axios.create({
+//     baseURL: import.meta.env.MODE ==="development"
+//     ? "http://localhost:4200/api/v1"
+//     :"/api/v1",
+//     withCredentials:true,
+
+// });
+
+
+
 import axios from "axios";
 
-export const axiosInstance=axios.create({
-    baseURL: import.meta.env.MODE ==="development"
-    ? "http://localhost:4200/api/v1"
-    :"/api/v1",
-    withCredentials:true,
-
+export const axiosInstance = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
+  withCredentials: true,
 });
-
