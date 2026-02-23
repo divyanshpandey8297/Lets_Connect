bb587b0fc3e5fa98354e16a873a2ef9af0b635bd
divyansh pandey <divyansh@divyanshs-MacBook-Air.local>
2026-02-24 02:12:59 +0530

    add the new method here

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -38,11 +38,16 @@
 
 import axios from "axios";
 
+// export const axiosInstance = axios.create({
+  // baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
+//   withCredentials: true,
+// });
+
+
 export const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
+  baseURL: "/api/v1",
   withCredentials: true,
 });
-
 // import axios from "axios";
 
 // export const axiosInstance = axios.create({
