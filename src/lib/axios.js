3f8a37a14c578c16a9ed4eb16568993575ce0a7e
divyansh pandey <divyansh@divyanshs-MacBook-Air.local>
2026-02-24 02:07:29 +0530

    change

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -39,6 +39,17 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://lets-connect-iru9.onrender.com/api/v1",
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
   withCredentials: true,
 });
+
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL 
+//     ? `${import.meta.env.VITE_API_URL}/api/v1`
+//     : import.meta.env.MODE === "development"
+//       ? "http://localhost:4200/api/v1"
+//       : "/api/v1",
+//   withCredentials: true,
+// });
